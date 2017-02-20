2224ce1be255caa427f084b335c13bf46c6af47c
monty <velimir.vesselinov@gmail.com>
2017-02-19 19:41:19 -0700

    better dynamic call added

--- src/SVR.jl
+++ src/SVR.jl
@@ -4,6 +4,8 @@ import JLD
 
 include("extras.jl")
 
+verbosity = false
+
 immutable svm_node
 	index::Cint
 	value::Cdouble
@@ -60,25 +62,48 @@ const RBF = Cint(2)
 const SIGMOID = Cint(3)
 const PRECOMPUTED = Cint(4)
 
-const svmlib = abspath(joinpath(Pkg.dir("SVR"), "deps", "libsvm.so.2"))
-const densesvmlib = abspath(joinpath(Pkg.dir("SVR"), "deps", "libdensesvm.so.2"))
+# const svmlib = abspath(joinpath(Pkg.dir("SVR"), "deps", "libsvm.so.2"))
+# const svmlib = abspath(joinpath(Pkg.dir("SVR"), "deps", "libdensesvm.so.2"))
 
-function mapnodes(instances)
-	nfeatures = size(instances, 1)
-	ninstances = size(instances, 2)
-	nodeptrs = Array(Ptr{svm_node}, ninstances)
-	nodes = Array(svm_node, nfeatures + 1, ninstances)
-	for i=1:ninstances
-		k = 1
-		for j=1:nfeatures
-			nodes[k, i] = svm_node(Cint(j), Float64(instances[j, i]))
-			k += 1
+# get library
+let libsvm = C_NULL
+	global get_lib
+	function get_lib()
+		if libsvm == C_NULL
+			libpath = joinpath(Pkg.dir("SVR"), "deps")
+			libfile = is_windows() ? oinpath(libpath, "libsvm$(Sys.WORD_SIZE).dll") : joinpath(libpath, "libsvm.so.2")
+			libsvm = Libdl.dlopen(libfile)
+			ccall(Libdl.dlsym(libsvm, :svm_set_print_string_function), Void, (Ptr{Void},), cfunction(liboutput, Void, (Ptr{UInt8},)))
 		end
-		nodes[k, i] = svm_node(Cint(-1), 0.0)
-		nodeptrs[i] = pointer(nodes, (i-1)*(nfeatures+1)+1)
+		libsvm
 	end
-	(nodes, nodeptrs)
 end
+
+# helper
+function liboutput(str::Ptr{UInt8})
+	if verbosity
+		print(unsafe_string(str))
+	end
+	nothing
+end
+
+# make lib function calls
+macro cachedsym(symname::Symbol)
+	cached = gensym()
+	quote
+		let $cached = C_NULL
+			global ($symname)
+			($symname)() = ($cached) == C_NULL ?
+				($cached = Libdl.dlsym(get_lib(), $(string(symname)))) :
+					$cached
+		end
+	end
+end
+
+@cachedsym svm_train
+@cachedsym svm_predict
+@cachedsym svm_save_model
+@cachedsym svm_free_model_content
 
 function mapparam(;
 	svm_type::Cint=EPSILON_SVR,
@@ -115,31 +140,64 @@ function mapparam(;
 	return param
 end
 
-function train(y::Vector, x::Array; dense::Bool=false, svm_type::Int32=EPSILON_SVR, kernel_type::Int32=RBF, degree::Integer=3, gamma::Float64=1/length(y), coef0::Float64=0.0, C::Float64=1.0, nu::Float64=0.5, p::Float64=0.1, cache_size::Float64=100.0, eps::Float64=0.001, shrinking::Bool=true, probability::Bool=false, verbose::Bool=false)
+function mapnodes(x::Array)
+	nfeatures = size(x, 1)
+	ninstances = size(x, 2)
+	nodeptrs = Array(Ptr{svm_node}, ninstances)
+	nodes = Array(svm_node, nfeatures + 1, ninstances)
+	for i=1:ninstances
+		for j=1:nfeatures
+			nodes[j, i] = svm_node(Cint(j), Float64(x[j, i]))
+		end
+		nodes[nfeatures + 1, i] = svm_node(Cint(-1), 0.0)
+		nodeptrs[i] = pointer(nodes, (i-1)*(nfeatures+1)+1)
+	end
+	(nodes, nodeptrs)
+end
+
+function train(y::Vector, x::Array; svm_type::Int32=EPSILON_SVR, kernel_type::Int32=RBF, degree::Integer=3, gamma::Float64=1/size(x, 1), coef0::Float64=0.0, C::Float64=1.0, nu::Float64=0.5, p::Float64=0.1, cache_size::Float64=100.0, eps::Float64=0.001, shrinking::Bool=true, probability::Bool=false, verbose::Bool=false)
 	param = mapparam(svm_type=svm_type, kernel_type=kernel_type, gamma=gamma, coef0=coef0, C=C, nu=nu, p=p, cache_size=cache_size, eps=eps, shrinking=shrinking, probability=probability)
 	(nodes, nodeptrs) = mapnodes(x)
 	prob = svm_problem(length(y), pointer(y), pointer(nodeptrs))
-	if !dense
-		pmodel = ccall((:svm_train, svmlib), Ptr{svm_model}, (Ptr{svm_problem},Ptr{svm_parameter}), pointer_from_objref(prob), pointer_from_objref(param))
-	else
-		pmodel = ccall((:svm_train, densesvmlib), Ptr{svm_model}, (Ptr{svm_problem},Ptr{svm_parameter}), pointer_from_objref(prob), pointer_from_objref(param))
-	end
+	pmodel = ccall(svm_train(), Ptr{svm_model}, (Ptr{svm_problem},Ptr{svm_parameter}), pointer_from_objref(prob), pointer_from_objref(param))
 	return pmodel
 end
 
-function predict(pmodel::Ptr{svm_model}, x::Array; dense::Bool=false)
+function predict(pmodel::Ptr{svm_model}, x::Array)
 	(nodes, nodeptrs) = mapnodes(x)
 	nx = size(x, 2)
 	y = Array(Float64, nx)
 	for i = 1:nx
-		if !dense
-			p = ccall((:svm_predict, svmlib), Float64, (Ptr{svm_model}, Ptr{svm_node}), pmodel, nodeptrs[i])
-		else
-			p = ccall((:svm_predict, densesvmlib), Float64, (Ptr{svm_model}, Ptr{svm_node}), pmodel, nodeptrs[i])
-		end
+		p = ccall(svm_predict(), Float64, (Ptr{svm_model}, Ptr{svm_node}), pmodel, nodeptrs[i])
 		y[i] = p
 	end
 	return y
+end
+
+function savemodel(pmodel::Ptr{svm_model}, filename::String)
+	ccall(svm_save_model(), Cint, (Ptr{UInt8}, Ptr{SVMModel}), file, pmodel)
+	nothing
+end
+
+function freemodel(pmodel::Ptr{svm_model})
+	ccall(svm_free_model_content(), Void, (Ptr{Void},), pmodel)
+	nothing
+end
+
+function readlibsvmfile(file::String)
+	d = readdlm(file)
+	(o, p) = size(d)
+	x = Array(Float64, o, p - 1)
+	y = []
+	try
+		y = Float64.(d[:,1])
+	catch
+		y = d[:,1]
+	end
+	for i = 2:p
+		x[:,i-1] = map(j->(parse(split(d[j,i], ':')[2])), collect(1:o))
+	end
+	return x, y
 end
 
 end
