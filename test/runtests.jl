f234d037f8e1952ac82f39db940177c76f61e90e
monty <velimir.vesselinov@gmail.com>
2017-05-14 17:36:38 -0600

    Base testing fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,19 +3,22 @@ import SVR
 currentdir = pwd()
 cd(dirname(@__FILE__))
 
-y_true = vec(readdlm("mg.result"))
+@Base.Test.testset "SVR" begin
 
-x, y = SVR.readlibsvmfile("mg.libsvm")
+    y_true = vec(readdlm("mg.result"))
 
-pmodel = SVR.train(y, x');
-y_pr = SVR.predict(pmodel, x');
-@assert maximum(abs.(y_pr .- y_true)) < 1e-4
-SVR.savemodel(pmodel, "mg.model")
-SVR.freemodel(pmodel)
+    x, y = SVR.readlibsvmfile("mg.libsvm")
 
-pmodel = SVR.loadmodel("mg.model")
-y_pr = SVR.predict(pmodel, x');
-# @assert maximum(abs.(y_pr .- y_true)) < 1e-4
-SVR.freemodel(pmodel)
+    pmodel = SVR.train(y, x');
+    y_pr = SVR.predict(pmodel, x');
+    @Base.Test.test isapprox(maximum(abs.(y_pr .- y_true)), 0, atol=1e-4)
+    SVR.savemodel(pmodel, "mg.model")
+    SVR.freemodel(pmodel)
+
+    pmodel = SVR.loadmodel("mg.model")
+    y_pr = SVR.predict(pmodel, x');
+    # @assert maximum(abs.(y_pr .- y_true)) < 1e-4
+    SVR.freemodel(pmodel)
+end
 
 cd(currentdir)
