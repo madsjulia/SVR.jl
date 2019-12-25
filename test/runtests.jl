6d558bd19e4b22a1ad70602f6853782c85acfb1c
monty <velimir.vesselinov@gmail.com>
2019-12-25 12:52:33 -0700

    clean up

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,18 @@
 import SVR
-using Test
-using DelimitedFiles
+import Test
+import DelimitedFiles
 
 currentdir = pwd()
 cd(dirname(@__FILE__))
 
-@testset "SVR" begin
-	y_true = vec(readdlm("mg.result"))
+@Test.testset "SVR" begin
+	y_true = vec(DelimitedFiles.readdlm("mg.result"))
 
 	x, y = SVR.readlibsvmfile("mg.libsvm")
 
 	pmodel = SVR.train(y, copy(permutedims(x)))
 	y_pr = SVR.predict(pmodel, copy(permutedims(x)))
-	@test isapprox(maximum(abs.(y_pr .- y_true)), 0, atol=1e-4)
+	@Test.test isapprox(maximum(abs.(y_pr .- y_true)), 0, atol=1e-4)
 	SVR.savemodel(pmodel, "mg.model")
 	SVR.freemodel(pmodel)
 
